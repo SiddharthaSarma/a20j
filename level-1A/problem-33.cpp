f0daaff88847ebc0e2090a0306486ba9d54062c5
Siddhartha <sidduinevitable@gmail.com>
2020-09-22 22:06:48 +0530

    cpp solution for codeforces 766 A.Mahmoud and Longest Uncommon Subsequence

---
+++ level-1A/problem-33.cpp
@@ -0,0 +1,18 @@
+// http://codeforces.com/contest/766/problem/A
+// 766/A. Mahmoud and Longest Uncommon Subsequence
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  string a, b;
+  cin >> a >> b;
+  if (a == b) {
+    cout << -1 << endl;
+  } else {
+    cout << max(a.size(), b.size()) << endl;
+  }
+  return 0;
+}
+
